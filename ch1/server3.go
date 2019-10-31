3690b8aeee56a3d886e374ff884922d792692c3c
Denislav Denov <denislavdenov@gmail.com>
2019-10-31 10:53:41 +0200

    adding code

--- ch1/server3.go
+++ ch1/server3.go
@@ -1,30 +1,78 @@
-// Server3 is an "echo" server that displays request parameters.
 package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
+// Packages not needed by version in book.
+
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	var cycles = 5 // number of complete x oscillator revolutions
+
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			cyclesStr := r.FormValue("cycles")
+			if cyclesStr != "" {
+				var err error
+				cycles, err = strconv.Atoi(cyclesStr)
+				if err != nil {
+					fmt.Fprintf(w, "bad cycles param: %s", cyclesStr, err)
+					return
+				}
+			}
+			lissajous(cycles, w)
+		}
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	lissajous(cycles, os.Stdout)
 }
 
-//!+handler
-// handler echoes the HTTP request.
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+func lissajous(cycles int, out io.Writer) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	palette := make([]color.Color, 0, nframes)
+	palette = append(palette, color.RGBA{0, 0, 0, 255})
+	for i := 0; i < nframes; i++ {
+		scale := float64(i) / float64(nframes)
+		c := color.RGBA{uint8(55 + 200*scale), uint8(55 + 200*scale), uint8(55 + 200*scale), 255}
+		palette = append(palette, c)
 	}
-	fmt.Fprintf(w, "Host = %q\n", r.Host)
-	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
-	if err := r.ParseForm(); err != nil {
-		log.Print(err)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8((i%(len(palette)-1))+1))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
 	}
-	for k, v := range r.Form {
-		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
-	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
